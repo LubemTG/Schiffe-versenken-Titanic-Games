982858b51f161e13c624196bb77645b2b3819e17
LubemTG <55669931+LubemTG@users.noreply.github.com>
2019-09-26 11:40:40 +0200

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.table.*;
+
+public class Array{
+  public void erstelleEigenesFeld(){
+    int [][] eA = new int [15][15];
+    }
+  }
